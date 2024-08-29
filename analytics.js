55a196bb39dc400ee271e9690db9ea768aec6bf5
Minghua Zhu <58354664+zhumingh@users.noreply.github.com>
2024-08-29 14:02:07 +0800

    Add files via upload
    add google analysis

---
+++ analytics.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-DJM7SC6MTC"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-DJM7SC6MTC');
+</script>
